272dbd30dc3f8db8c9fef954ac24521f089b7089
SaadKc <saadalikc02@gmail.com>
2023-07-03 06:05:32 +0500

    remove tab

--- lib/screens/mainScreens/main_screen.dart
+++ lib/screens/mainScreens/main_screen.dart
@@ -3,30 +3,26 @@ import 'package:rider/tabPages/earning_tab.dart';
 import 'package:rider/tabPages/home_tab.dart';
 import 'package:rider/tabPages/ratings_tab.dart';
 
-
 import '../../tabPages/profile_tab.dart';
 import '../profile screen/profile_screen.dart';
 
-class MainScreen extends StatefulWidget
-{
+class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
   @override
   State<MainScreen> createState() => _MainScreenState();
 }
 
-
-class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateMixin
-{
+class _MainScreenState extends State<MainScreen>
+    with SingleTickerProviderStateMixin {
   TabController? tabController;
-  int selectedIndex =0;
+  int selectedIndex = 0;
 
-onItemClicked(int index)
-{
-  setState(() {
-    selectedIndex = index;
-    tabController!.index = selectedIndex;
-  });
-}
+  onItemClicked(int index) {
+    setState(() {
+      selectedIndex = index;
+      tabController!.index = selectedIndex;
+    });
+  }
 
   @override
   void initState() {
@@ -34,48 +30,42 @@ onItemClicked(int index)
     tabController = TabController(length: 4, vsync: this);
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body: TabBarView(
-       physics: const NeverScrollableScrollPhysics(),
-       controller: tabController,
-       children: const [
-         HomeTabPage(),
-         EarningTabPage(),
-         RaitingsTabPage(),
-         ProfileScreen(),
-       ],
-     ),
-       bottomNavigationBar:
-       BottomNavigationBar(
-         items: const [
-           BottomNavigationBarItem(icon: Icon(Icons.home),
-           label: "Home",
+      body: TabBarView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: tabController,
+        children: const [
+          HomeTabPage(),
+          EarningTabPage(),
+          ProfileScreen(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
           ),
-           BottomNavigationBarItem(icon: Icon(Icons.credit_card),
-             label: "location",
-           ),
-           BottomNavigationBarItem(icon: Icon(Icons.star),
-             label: "Raitings",
-           ),
-           BottomNavigationBarItem(icon: Icon(Icons.home),
-             label: "Account",
-           ),
-      ],
-
-
-         unselectedItemColor: Colors.white54,
-         selectedItemColor: Colors.white,
-         backgroundColor: Colors.black,
-         type: BottomNavigationBarType.fixed,
-         selectedLabelStyle: const TextStyle(fontSize: 14),
-         showUnselectedLabels: true,
-         currentIndex: selectedIndex,
-         onTap: onItemClicked,
-     ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.credit_card),
+            label: "location",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Account",
+          ),
+        ],
+        unselectedItemColor: Colors.white54,
+        selectedItemColor: Colors.white,
+        backgroundColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: const TextStyle(fontSize: 14),
+        showUnselectedLabels: true,
+        currentIndex: selectedIndex,
+        onTap: onItemClicked,
+      ),
     );
   }
 }
